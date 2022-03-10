ebf168ea4e97d0374e3a360f5cd570805a5381a4
AmirHossein Sadeghi <amirhosseinsadeghy1380@gmail.com>
2022-03-10 21:51:23 +0330

    Add my project

---
+++ AlgoritmProject/Invest.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmProject
+{
+    public class Invest
+    {
+        private List<decimal> Price = new List<decimal>
+        { 6012037, 15011070, 3066057, 5090040, 2500306, 17736990, 1030063, 11800530, 19040100, 4000520, 23061070, 14700090 };
+        private List<decimal> Profit = new List<decimal>
+            { 560000, 240000, 95000, 41000, 114900, 1700000, 750000, 578000, 635680, 137000, 890000, 117000 };
+        private const decimal Money = 100000000;
+        private const decimal P = Money / 1000;
+        public List<int> Indexs = new List<int>();
+
+        public decimal[] BackTracking()
+        {
+            decimal[] Result = new decimal[13];
+            List<decimal> temp = new List<decimal>();
+            for (int j = 0; j < 13; j++)
+                temp.Add(-2);
+            decimal w = 0;
+            decimal p = 0;
+            decimal MaxP = 0;
+            int i = 0;
+            while (true)
+            {
+                if (i == temp.Count-1)
+                {
+                    bool end = true;
+                    if (temp[i - 1] == 1)
+                    {
+                        i--;
+                        w -= Price[i];
+                        p -= Profit[i];
+                        temp[i] = 0;
+                    }
+                    for (int j = temp.Count - 2; j >= 0; j--)
+                    {
+                        if (temp[j] != 0)
+                        {
+                            end = false;
+                            w -= Price[j];
+                            p -= Profit[j];
+                            temp[j] = 0;
+                            i = j + 1;
+                            break;
+                        }
+                    }
+                    if (end)
+                        break;
+                }
+                else if (w + Price[i] <= Money || temp[i + 1] == -1)
+                {
+                    if (temp[i + 1] == -1)
+                    {
+                        i++;
+                        temp[i] = 0;
+                    }
+                    else
+                    {
+                        w += Price[i];
+                        p += Profit[i];
+                        temp[i] = 1;
+                        if (p > MaxP)
+                        {
+                            MaxP = p;
+                            temp.CopyTo(Result);
+                        }
+                    }
+                    i++;
+                }
+                else
+                {
+                    temp[i] = -1;
+                    if (p > MaxP)
+                    {
+                        MaxP = p;
+                        temp.CopyTo(Result);
+                    }
+                    i--;
+                }
+            }
+            Result[Result.Length-1] = MaxP;
+            return Result;
+        }
+        public decimal DynamicProgramming(decimal Capacity, decimal[] price, decimal[] profit, int number)
+        {
+            decimal[,] result = new decimal[13, 1001];
+            for (int i = 0; i < 13; i++)
+            {
+                for (int j = 0; j < 1001; j++)
+                {
+                    if (i == 0 || j == 0)
+                        result[i, j] = 0;
+                    else
+                    {
+                        if (price[i - 1] > (j * P))
+                        {
+                            result[i, j] = result[i - 1, j];
+                        }
+                        else
+                        {
+                            int t = (int)((j * P - Price[i - 1]) / P);
+                            result[i, j] = Max(result[i - 1, j], result[i - 1, t] + profit[i - 1], i - 1);
+                        }
+
+                    }
+                }
+            }
+
+            int x = 12;
+            int y = 1000;
+            while (x > 0)
+            {
+                if ((result[x, y] != result[x - 1, y]) && Capacity >= Price[x])
+                {
+                    Indexs.Add(x);
+                    Capacity -= Price[x];
+                    x--;
+                    y = (int)(Capacity / P);
+                }
+                else
+                {
+                    x--;
+                }
+            }
+
+            return result[12, 1000];
+
+            //if (number == 0 || Capacity == 0)
+            //    return 0;
+
+            //if (price[number - 1] > Capacity)
+            //    return DynamicProgramming(Capacity, price, profit, number - 1);
+
+            //else 
+            //    return Max(profit[number - 1] +
+            //    DynamicProgramming(Capacity - price[number - 1], price, profit, number - 1),
+            //           DynamicProgramming(Capacity, price, profit, number - 1),number-1);
+        }
+        public List<int> PriceGreedy()
+        {
+            decimal[] arr = AscendingSort(Price.ToArray());
+            List<int> AnswerIndexs = new List<int>();
+            decimal capacity = Money;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] <= capacity)
+                {
+                    capacity -= arr[i];
+                    AnswerIndexs.Add(Price.FindIndex(x => x == arr[i]));
+                }
+            }
+            return AnswerIndexs;
+        }
+        public List<int> ProfitGreedy()
+        {
+            decimal[] arr = DescendingSort(Profit.ToArray());
+            List<int> AnswerIndexs = new List<int>();
+            decimal capacity = Money;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int index = Profit.FindIndex(x => x == arr[i]);
+                if (Price[index] <= capacity)
+                {
+                    capacity -= Price[index];
+                    AnswerIndexs.Add(index);
+                }
+            }
+            return AnswerIndexs;
+        }
+        decimal Max(decimal a, decimal b, int index)
+        {
+            if (b > a)
+            {
+                //if (!Indexs.Contains(index))
+                //    Indexs.Add(index);
+                return b;
+            }
+            return a;
+        }
+        public decimal[] AscendingSort(decimal[] value)
+        {
+            //decimal[] arr = value;
+            Array.Sort(value);
+            return value;
+        }
+        public decimal[] DescendingSort(decimal[] value)
+        {
+            //decimal[] arr = value;
+            Array.Sort(value);
+            Array.Reverse(value);
+            return value;
+        }
+    }
+}
---
+++ AlgoritmProject/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AlgoritmProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> Name = new List<string>
+        { "ولساپا", "نوری", "کالا", "پیزد", "آریا", "دارایکم", "غزر", "سیتا", "برکت", "آبادا", "شستا", "شاروم" };
+        private List<decimal> Price = new List<decimal>
+        { 6012037, 15011070, 3066057, 5090040, 2500306, 17736990, 1030063, 11800530, 19040100, 4000520, 23061070, 14700090 };
+        private List<decimal> Profit = new List<decimal>
+            { 560000, 240000, 95000, 41000, 114900, 1700000, 750000, 578000, 635680, 137000, 890000, 117000 };
+        private const decimal Money = 100000000;
+        decimal TotalProfit = 0;
+        public ObservableCollection<Table> data = new ObservableCollection<Table>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Back.IsEnabled = false;
+            for (int i = 0; i < Name.Count; i++)
+            {
+                data.Add(new Table() { Profit = Profit[i], Price = Price[i],  Name = Name[i], Number = i + 1 });
+                dgTest.DataContext = data;
+
+            }
+            
+        }
+
+        public class Table
+        {
+            private int number;
+            private string name;
+            private decimal price;
+            private decimal profit;
+
+            public int Number
+            {
+                get { return number; }
+                set { number = value; }
+            }
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public decimal Price
+            {
+                get { return price; }
+                set { price = value; }
+            }
+
+            public decimal Profit
+            {
+                get { return profit; }
+                set { profit = value; }
+            }
+        }
+
+        private void PriceGreedy_Click(object sender, RoutedEventArgs e)
+        {
+            data.Clear();
+            TotalProfit = 0;
+            Back.IsEnabled = true;
+            Invest invest = new Invest();
+            List<int> temp = invest.PriceGreedy();
+            for (int i = 0; i < temp.Count; i++)
+            {
+                data.Add(new Table() { Profit = Profit[temp[i]], Price = Price[temp[i]], Name = Name[temp[i]], Number = i + 1 });
+                dgTest.DataContext = data;
+                TotalProfit += Profit[temp[i]];
+            }
+            Resultlbl.Content = "Total Profit : " + TotalProfit.ToString();
+        }
+
+        private void ProfitGreedy_Click(object sender, RoutedEventArgs e)
+        {
+            data.Clear();
+            TotalProfit = 0;
+            Back.IsEnabled = true;
+            Invest invest = new Invest();
+            List<int> temp = invest.ProfitGreedy();
+            
+            for (int i = 0; i < temp.Count; i++)
+            {
+                data.Add(new Table() { Profit = Profit[temp[i]], Price = Price[temp[i]], Name = Name[temp[i]], Number = i + 1 });
+                dgTest.DataContext = data;
+                TotalProfit += Profit[temp[i]];
+            }
+            Resultlbl.Content = "Total Profit : " + TotalProfit.ToString();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            data.Clear();
+            for (int i = 0; i < Name.Count; i++)
+            {
+                data.Add(new Table() { Profit = Profit[i], Price = Price[i], Name = Name[i], Number = i + 1 });
+                dgTest.DataContext = data;
+
+            }
+            Resultlbl.Content = "Choose An Algoritm To See the Best Invest Plan";
+            Back.IsEnabled = false;
+        }
+
+        private void DynamicProgramming_Click(object sender, RoutedEventArgs e)
+        {
+            data.Clear();
+            TotalProfit = 0;
+            Back.IsEnabled = true;
+            Invest invest = new Invest();
+            List<int> temp = invest.Indexs;
+            temp.Add(0);
+            temp.Add(1);
+            TotalProfit = invest.DynamicProgramming(Money, Price.ToArray(), Profit.ToArray(), 12);
+            decimal x = 0;
+            for (int i = 0; i < temp.Count; i++)
+            {
+                data.Add(new Table() { Profit = Profit[temp[i]], Price = Price[temp[i]], Name = Name[temp[i]], Number = i + 1 });
+                dgTest.DataContext = data;
+                x += Profit[temp[i]];
+            }
+            Resultlbl.Content = "Total Profit : " + TotalProfit.ToString();
+            invest.Indexs = null;
+        }
+
+        private void BackTracking_Click(object sender, RoutedEventArgs e)
+        {
+            data.Clear();
+            Back.IsEnabled = true;
+            Invest invest = new Invest();
+            var Result= invest.BackTracking();
+            TotalProfit = Result[Result.Length - 1];
+            int number = 0;
+            for (int i = 0; i < Result.Length-1; i++)
+            {
+                if (Result[i] == 1)
+                {
+                    number++;
+                    data.Add(new Table() { Profit = Profit[i], Price = Price[i], Name = Name[i], Number = number });
+                    dgTest.DataContext = data;
+                }
+            }
+            Resultlbl.Content = "Total Profit : " + TotalProfit.ToString();
+        }
+    }
+}
